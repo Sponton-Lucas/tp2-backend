1ce8e07b82b1c013638bae4a7e16a2213c3780a4
Ivan <inolasco@fi.uba.ar>
2026-04-05 19:57:27 -0300

    prueba de push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola, Flask está funcionando!"
+    return "¡Hola, Flask está funcionando! y pude hacer un pull"
 
 if __name__ == "__main__":
     app.run(debug=True)
