24c503b0833c9eb9830e4988fb85646839cebc30
AgustinAntonic <aantonic@fi.uba.ar>
2026-04-05 23:28:06 -0300

    hice lo de crear partido por el post. despues les mando como mandar un post por terminal

--- app.py
+++ app.py
@@ -16,7 +16,21 @@ def get_partido(id):
     else:
         return jsonify({'error': 'No se encuentra el partido'}), 404
 
+@app.route('/partidos', methods=['POST'])
+def post_partido():
+    partido = request.get_json()
+    if (not partido) or ("equipo_local" not in partido) or ("equipo_visitante" not in partido) or ("fecha" not in partido) or ("fase" not in partido):
+        return jsonify({'error': 'Algo falta'}), 400
+    else:
+        equipo_local = partido.get("equipo_local")
+        equipo_visitante = partido.get("equipo_visitante")
+        fecha = partido.get("fecha")
+        fase = partido.get("fase")
 
+        nuevo_partido = db.crear_partido(equipo_local, equipo_visitante, fecha, fase)
+        return jsonify(nuevo_partido), 201
+        
+        
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
--- db.py
+++ db.py
@@ -16,3 +16,30 @@ def get_partido_por_id(id):
         for i in range(len(partidos)):
             if partidos[i]["id"] == id_ingresado:
                 return partidos[i]
+
+def crear_partido(equipo_local, equipo_visitante, fecha, fase):
+    partido = {
+    "id": "",
+    "equipo_local": "",
+    "equipo_visitante": "",
+    "estadio": "",
+    "ciudad": "",
+    "fecha": "",
+    "fase": ""
+    }
+
+    partido["equipo_local"] = equipo_local
+    partido["equipo_visitante"] = equipo_visitante
+    partido["fecha"] = fecha
+    partido["fase"] = fase
+    
+    with open("data/partidos.csv", "r") as archivo:
+        lector = csv.DictReader(archivo)
+        contenido = list(lector)
+        max_id = len(contenido)
+    partido["id"] = max_id + 1
+
+    with open("data/partidos.csv", "a") as archivo:
+        writer = csv.DictWriter(archivo, fieldnames=partido.keys())
+        writer.writerow(partido)
+    return partido
